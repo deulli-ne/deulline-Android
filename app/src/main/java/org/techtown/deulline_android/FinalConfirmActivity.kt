042185ef37828db8a649141c46e8e3c14edeed51
yujeongkimm <20201111@duksung.ac.kr>
2022-08-11 00:12:12 +0900

    Feat:화면 UI 구성

---
+++ app/src/main/java/org/techtown/deulline_android/FinalConfirmActivity.kt
@@ -0,0 +1,11 @@
+package org.techtown.deulline_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FinalConfirmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_final_confirm)
+    }
+}

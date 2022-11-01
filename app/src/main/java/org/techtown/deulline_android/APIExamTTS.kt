3ef083d4d8f6c5068e98a35431e1d194102d713c
yujeongkimm <20201111@duksung.ac.kr>
2022-11-01 23:11:35 +0900

    Fix: CLOVA VOICE 실행

--- app/src/main/java/org/techtown/deulline_android/APIExamTTS.kt
+++ app/src/main/java/org/techtown/deulline_android/APIExamTTS.kt
@@ -1,8 +1,12 @@
 package org.techtown.deulline_android
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.media.MediaPlayer;
+import android.os.Build
 import android.os.Environment;
 import android.util.Log;
+import androidx.annotation.RequiresApi
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -19,23 +23,25 @@ import java.net.URLEncoder;
 //https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=jcosmoss&logNo=220963959630
 class APIExamTTS {
     private val clientId = "6yn3c2ogrm" //애플리케이션 클라이언트 아이디값";
-    private val clientSecret = "IQFkWlNH38ztsmaccxqGimlLDhsMALddT494huvg" //애플리케이션 클라이언트 시크릿값";
+    private val clientSecret = "zrRMhqOBZ6m4XXT69h1UzLRDjhh0wXGbjfPxK8Jl" //애플리케이션 클라이언트 시크릿값";
 
+    @SuppressLint("NewApi")
     fun main(args: Array<out Array<String?>?>) {
         val clientId = clientId
         val clientSecret = clientSecret
         try {
             Log.d("TTS", args[0]!![0].toString())
-            val text: String = URLEncoder.encode(args[0].toString(), "UTF-8") // 13자
-            val apiURL = "https://openapi.naver.com/v1/voice/tts.bin"
+            val text: String = URLEncoder.encode(args[0]!![0].toString(), "UTF-8") // 13자
+            val apiURL = "https://naveropenapi.apigw.ntruss.com/tts-premium/v1/tts"
             val url = URL(apiURL)
+
             val con: HttpURLConnection = url.openConnection() as HttpURLConnection
             con.setRequestMethod("POST")
-            con.setRequestProperty("X-Naver-Client-Id", clientId)
-            con.setRequestProperty("X-Naver-Client-Secret", clientSecret)
+            con.setRequestProperty("X-NCP-APIGW-API-KEY-ID", clientId)
+            con.setRequestProperty("X-NCP-APIGW-API-KEY", clientSecret)
 
             // post request
-            val postParams = "speaker=mijin&speed=0&text=$text"
+            val postParams = "speaker=nara&speed=0&format=mp3&text=$text"
             con.setDoOutput(true)
             con.setDoInput(true)
 
@@ -48,29 +54,39 @@ class APIExamTTS {
             val responseCode: Int = con.getResponseCode()
             val br: BufferedReader
 
+            Log.d("TTS", responseCode.toString())
+            Log.d("TTS", con.responseMessage.toString())
+
             if (responseCode == 200) { // 정상 호출
+                Log.d("TTS", "정상 호출")
                 val `is`: InputStream = con.getInputStream()
                 var read = 0
                 val bytes = ByteArray(1024)
-                //폴더 만듬. 없으면 새로 생성. 일단 Naver폴더에 저장
-                val dir = File(Environment.getExternalStorageDirectory().toString() + "/", "Naver")
-                if (!dir.exists()) {
-                    dir.mkdirs()
+
+                //val dir = File("/storage/emulated/0/Download")
+                val dir = File("/sdcard/Download")
+                if(!dir.exists()){
+                    dir.mkdir()
                 }
 
                 val tempname = "naverttstemp" //하나의 파일명으로 덮어씀
-                val f =
-                    File(tempname + ".mp3")
+                //val f = File("/storage/emulated/0/Download/" + tempname + ".mp3")
+                val f = File("/sdcard/Download/" + tempname + ".mp3")
                 f.createNewFile()
 
                 val outputStream: OutputStream = FileOutputStream(f)
+
                 while (`is`.read(bytes).also { read = it } != -1) {
                     outputStream.write(bytes, 0, read)
                 }
                 `is`.close()
 
                 //mp3파일 재생
-                val Path_to_file: String = tempname + ".mp3"
+                //val Path_to_file = "/storage/emulated/0/Download/" + tempname + ".mp3"
+                val Path_to_file = "/sdcard/Download/" + tempname + ".mp3"
+
+                Log.d("TTS", Path_to_file)
+
                 val audioPlay = MediaPlayer()
 
                 audioPlay.setDataSource(Path_to_file)
